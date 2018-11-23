6e72e0fe8ce6551d0b81b3750ad88c4d4778eb4e
Diego Muracciole <diegomuracciole@gmail.com>
2018-11-22 23:57:34 -0300

    Fix build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
-import babel from 'rollup-plugin-babel';
-import json from 'rollup-plugin-json';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import bundleSize from 'rollup-plugin-bundle-size';
-import uglify from 'rollup-plugin-uglify';
-import string from 'rollup-plugin-string';
+import babel from 'rollup-plugin-babel'
+import json from 'rollup-plugin-json'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import bundleSize from 'rollup-plugin-bundle-size'
+import uglify from 'rollup-plugin-uglify'
+import string from 'rollup-plugin-string'
 import replace from 'rollup-plugin-replace'
 import ignore from 'rollup-plugin-ignore'
-import pkg from './package.json';
+import pkg from './package.json'
 
 const cjs = {
   exports: 'named',
@@ -28,43 +28,42 @@ const configBase = {
     string({ include: '**/*.afm' }),
     babel({
       babelrc: false,
-      presets: [['es2015', { modules: false }]],
+      presets: [
+        [
+          'env',
+          {
+            modules: false,
+            targets: {
+              ie: '11'
+            }
+          }
+        ]
+      ],
       plugins: ['external-helpers'],
       runtimeHelpers: true
     }),
-    bundleSize(),
+    bundleSize()
   ],
   external: Object.keys(pkg.dependencies)
 }
 
 const serverConfig = Object.assign({}, configBase, {
-  output: [
-    getESM({ file: 'dist/pdfkit.es.js' }),
-    getCJS({ file: 'dist/pdfkit.cjs.js' }),
-  ],
+  output: [getESM({ file: 'dist/pdfkit.es.js' }), getCJS({ file: 'dist/pdfkit.cjs.js' })],
   plugins: configBase.plugins.concat(
     replace({
-      BROWSER: JSON.stringify(false),
+      BROWSER: JSON.stringify(false)
     })
   ),
   external: configBase.external.concat(['fs'])
 })
 
 const serverProdConfig = Object.assign({}, serverConfig, {
-  output: [
-    getESM({ file: 'dist/pdfkit.es.min.js' }),
-    getCJS({ file: 'dist/pdfkit.cjs.min.js' }),
-  ],
-  plugins: serverConfig.plugins.concat(
-    uglify()
-  ),
+  output: [getESM({ file: 'dist/pdfkit.es.min.js' }), getCJS({ file: 'dist/pdfkit.cjs.min.js' })],
+  plugins: serverConfig.plugins.concat(uglify())
 })
 
 const browserConfig = Object.assign({}, configBase, {
-  output: [
-    getESM({ file: 'dist/pdfkit.browser.es.js' }),
-    getCJS({ file: 'dist/pdfkit.browser.cjs.js' }),
-  ],
+  output: [getESM({ file: 'dist/pdfkit.browser.es.js' }), getCJS({ file: 'dist/pdfkit.browser.cjs.js' })],
   plugins: configBase.plugins.concat(
     replace({
       BROWSER: JSON.stringify(true)
@@ -74,18 +73,8 @@ const browserConfig = Object.assign({}, configBase, {
 })
 
 const browserProdConfig = Object.assign({}, browserConfig, {
-  output: [
-    getESM({ file: 'dist/pdfkit.browser.es.min.js' }),
-    getCJS({ file: 'dist/pdfkit.browser.cjs.min.js' }),
-  ],
-  plugins: browserConfig.plugins.concat(
-    uglify()
-  ),
+  output: [getESM({ file: 'dist/pdfkit.browser.es.min.js' }), getCJS({ file: 'dist/pdfkit.browser.cjs.min.js' })],
+  plugins: browserConfig.plugins.concat(uglify())
 })
 
-export default [
-  serverConfig,
-  serverProdConfig,
-  browserConfig,
-  browserProdConfig
-]
+export default [serverConfig, serverProdConfig, browserConfig, browserProdConfig]
